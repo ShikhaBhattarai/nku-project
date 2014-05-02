f3b4ac3ddd08e6a5b3c6d85f06a0c21f16817377
shikhabhattarai <bhattarais1@gmail.com>
2014-05-02 10:52:08 +0000

    Add Create Recipie form

--- app/controllers/recipies_controller.rb
+++ app/controllers/recipies_controller.rb
@@ -1,11 +1,26 @@
 class RecipiesController < ApplicationController
   
+  
   def index
     if params[:search].nil?
       @recipies = Recipie.where(:category => params["format"])
     else
       @recipies = Recipie.search(params[:search])
     end
+  end
+  
+  def new
+    @recipie = Recipie.new
+    
+  end
+  
+  def create
+   
+    @recipie = Recipie.new(recipie_params.except("ingredients"))
+    @recipie.flavors =  params[:flavors]
+    @recipie.ingredients = params["recipie"]["ingredients"].split(",") 
+    @recipie.save
+
   end
   
   def show    
@@ -21,8 +36,14 @@ class RecipiesController < ApplicationController
     redirect_to recipy_path(params[:comment][:recipe_id])
   end
   
+  private
+  
   def comment_params
     params.require(:comment).permit!
+  end
+  
+  def recipie_params
+    params.require(:recipie).permit!  
   end
   
 end
--- app/models/recipie.rb
+++ app/models/recipie.rb
@@ -5,7 +5,7 @@ class Recipie < ActiveRecord::Base
   serialize :attributes, Hash
   serialize :flavors, Hash
   serialize :ingredients, Array
-
+  
   def self.search(search)
     if search
       find(:all, :conditions => ['recipeName LIKE ?', "%#{search}%"])
